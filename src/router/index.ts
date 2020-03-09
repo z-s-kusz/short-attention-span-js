1b5932e7d7327902e4ddde20f9dbbeb99f4a2eb1
Zach Kusz <z.s.kusz@gmail.com>
2020-03-08 20:38:40 -0500

    Added color golf to router and link.

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/color-golf',
+    name: 'ColorGolf',
+    component: () => import('../views/ColorGolf.vue'),
+  },
 ];
 
 const router = new VueRouter({
