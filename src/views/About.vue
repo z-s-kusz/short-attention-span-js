c764359484e04507695e1ea20469beed03646c5e
Zach Kusz <z.s.kusz@gmail.com>
2020-06-22 21:23:20 -0500

    Updated about, removed blank home page route.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is my first Vue.js project!
+      It's just intended as a quick way to try out Vue.
+      I hope to update it with more small games and interactive time wasters.</h1>
   </div>
 </template>
