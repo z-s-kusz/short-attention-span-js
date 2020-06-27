8e987ec1d2755a6d4cc9865d477b9a45d30b3983
Zach Kusz <z.s.kusz@gmail.com>
2020-06-27 13:45:58 -0500

    WIP added carousel view and static styles.

---
+++ src/components/mgs-carousel/MgsCarousel.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="container">
+    <div v-for="(item, i) in items" :key="i"
+      class="item" :class="`class${i}`">
+      <div class="black-box-left"></div>
+      <div v-html="item.img" class="item-image"></div>
+      <div class="black-box-right"></div>
+      <!-- <span>{{ item.description }}</span>
+      <span class="item-name">{{ item.name }}</span> -->
+      </div>
+  </div>
+</template>
+
+<script>
+const items = [
+  { name: 'Star', description: '', img: '&#10029;' },
+  { name: 'Shamrock', description: 'Lucky', img: '&#9752;' },
+  { name: 'Peace', description: '', img: '&#9774;' },
+  { name: 'Sun', description: '5,778 K', img: '&#9728;' },
+  { name: 'Pawns', description: '5/8', img: '&#9823;' },
+  { name: 'Notes', description: '4/4', img: '&#9835;' },
+  { name: 'Snow', description: '', img: '&#10053;' },
+  { name: 'Arrows', description: '3/10', img: '&#10163;' },
+  { name: 'Phone;', description: 'Call Me', img: '&phone;' },
+];
+export default {
+  name: 'MgsCarousel',
+  data() {
+    return {
+      items,
+      previous3: items.length - 3,
+      previous2: items.length - 2,
+      previous1: items.length - 1,
+      selectedItemIndex: 0,
+      next1: 1, // todo dynamicly set these...
+      next2: 2,
+      next3: 3,
+    };
+  },
+  methods: {
+    getPreviousItems(index) {
+      return index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 25% 75%;
+  grid-template-rows: 78% 22%;
+  justify-content: center;
+  align-items: flex-end;
+  background-color: rgba(255, 255, 255, 0.4);
+}
+.item-image {
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  grid-row: 1 / 2;
+  grid-column: 2 / 3;
+  border-top: 2px solid black;
+  border-right: 6px solid black;
+  font-size: 74px;
+  color: purple;
+}
+.item-name {
+  font-size: 16px;
+  font-weight: 600;
+  color: rgba(25, 165, 170, 0.6);
+  background-color: rgba(1, 1, 1, 0.3);
+}
+.black-box-left {
+  grid-row: 1 / 3;
+  grid-column: 1 / 2;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+}
+.black-box-right {
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+}
+
+
+.container {
+  background-image: url('https://www.marylandzoo.org/wp-content/uploads/2019/10/penguinslider2.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: initial;
+  display: grid;
+  grid-template-rows: repeat(5, 100px);
+  grid-template-columns: repeat(5, 160px);
+  grid-gap: 20px 20px;
+  /* Keep boxes in lower left */
+  justify-content: left;
+  align-content: end;
+}
+
+.selected, .class0 {
+  grid-row: 5 / 6;
+  grid-column: 1 / 2;
+  color: orangered;
+}
+
+.previous1, .class6 {
+  grid-row: 4 / 5;
+  grid-column: 1 / 2;
+}
+.previous2, .class7 {
+  grid-row: 3 / 4;
+  grid-column: 1 / 2;
+}
+.previous3, .class8 {
+  grid-row: 2 / 3;
+  grid-column: 1 / 2;
+}
+
+.next1, .class1 {
+  grid-row: 5 / 6;
+  grid-auto-columns: 2 / 3;
+}
+.next2, .class2 {
+  grid-row: 5 / 6;
+  grid-auto-columns: 3 / 4;
+}
+.next3, .class3 {
+  grid-row: 5 / 6;
+  grid-auto-columns: 4 / 5;
+}
+
+.hide, .class4, .class5 {
+  display: none;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    redirect: 'color-golf',
+    component: Home,
   },
   {
     path: '/about',
