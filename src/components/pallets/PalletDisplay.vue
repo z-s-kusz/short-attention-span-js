d477c97ef985859c9594c2cf93a654596bdcd9e1
Zach Kusz <z.s.kusz@gmail.com>
2020-08-20 21:59:49 -0500

    Thined borders, set size.

--- src/components/pallets/PalletDisplay.vue
+++ src/components/pallets/PalletDisplay.vue
@@ -1,15 +1,14 @@
 <template>
-    <svg width="300" viewBox="0 0 19 7">
+    <svg width="220" viewBox="0 0 18.5 6.5">
       <rect v-for="(color, i) in colors" :key="i"
-        :x="i * 3 + 0.5" y="0.5" width="9" height="6" :fill="color"
+        :x="i * 3 + 0.25" y="0.25" width="9" height="6" :fill="color"
         :stroke="color" stroke-width="0" ry="3" rx="3" />
-      <rect x="0.5" y="0.5" width="18" height="6" fill-opacity="0"
-        stroke="white" stroke-width="1" ry="3" rx="3" />
+      <rect x="0.25" y="0.25" width="18" height="6" fill-opacity="0"
+        stroke="white" stroke-width="0.25" ry="3" rx="3" />
     </svg>
 </template>
 
 <script>
-// TODO thiner border
 export default {
   name: 'PalleteDisplay',
   // props: {
