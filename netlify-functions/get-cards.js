911a9d05e5dd734a5b85254905a2da35efd9947b
Zach Kusz <z.s.kusz@gmail.com>
2021-02-19 16:28:34 -0600

    wip - added get cards, connected azure cosmosdb

---
+++ netlify-functions/get-cards.js
@@ -0,0 +1,28 @@
+const mongoClient = require('mongodb').MongoClient;
+const connectionString = process.env.COSMOS_DB_PRIMARY_CONNECTION_STRING;
+
+exports.handler = async (event, context) => {
+  return mongoClient.connect(connectionString, (connectError, client) => {
+    if (connectError) {
+      console.error('connection error:', connectError);
+      return {
+        statusCode: 500,
+        body: 'Error establishing mongo client connection.',
+      };
+    }
+
+    try {
+      const cards = await client.db('monstermon').collection('Cards').find().toArray();
+      return {
+        statusCode: 200,
+        body: cards,
+      };
+    } catch (readError) {
+      console.error('read error:', readError);
+      return {
+        statusCode: 500,
+        body: 'Error reading cards documents.',
+      };
+    }
+  });
+};
